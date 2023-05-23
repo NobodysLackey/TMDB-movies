047581540dd96fe1f0bc32f6c845b5f1bc3db3db
Michael Lackey <michael.g.lackey@gmail.com>
2023-05-23 14:28:48 -0500

    add solution

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,13 +5,13 @@ const DOMAIN = 'https://api.themoviedb.org/3'
 const axios = require('axios')
 
 const index = async (req, res) => {
-  const response = await axios.get(`${DOMAIN}/movie/popular?language=en-US&api_key=${API_KEY}`)
+  const response = await axios.get(`${DOMAIN}/movie/popular?include_adult=false&language=en-US&api_key=${API_KEY}`)
   const movies = response.data.results
   res.render('movies', { movies })
 }
 
 const search = async (req, res) => {
-  const response = await axios.get(`${DOMAIN}/search/movie?query=${req.query.movie}&api_key=${API_KEY}`)
+  const response = await axios.get(`${DOMAIN}/search/movie?include_adult=false&query=${req.query.movie}&api_key=${API_KEY}`)
   const movies = response.data.results
   res.render('movies', { movies })
 }
